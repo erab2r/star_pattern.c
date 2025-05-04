66ecd61898aae8bc20f725ac61c6a9c5ea5ef14c
erab2r <shkera213@gmail.com>
2025-05-05 04:25:03 +0600

    second

--- star_pattern3.c
+++ star_pattern3.c
@@ -2,17 +2,17 @@
 int main(){
     int i,j,n;
     scanf("%d",&n);
-    int star = 1;
+    int val = 1;
     int space = n-1;
     for(i=1;i<=n;i++){
         for(j=1;j<=space;j++){
             printf(" ");
         }
-        for(j=1;j<=star;j++){
-            printf("*");
+        for(j=1;j<=val;j++){
+            printf("%d ",j);
         }
         printf("\n");
-        star ++;
+        val++;
         space--;
     }
 
